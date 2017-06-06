c367106bc9a5e321ac9126927afb6db3c17e414e
SimonDevon <simon@simondevon.co.uk>
2017-06-06 01:36:10 +0100

    add triangle script

---
+++ triangle1.py
@@ -0,0 +1,16 @@
+# Step 1: Make all the "turtle" commands available to us.
+import turtle
+
+# Step 2: create a new turtle, we'll call him bob and make him look like a turtle
+bob = turtle.Turtle()
+bob.shape("turtle")
+
+# Step 3: lets make Bob draw a red star
+bob.color("blue")
+
+# Lets draw a star!
+for i in range(3):
+    bob.forward(200)
+    bob.left(120)
+    
+
