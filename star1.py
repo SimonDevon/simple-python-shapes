6a04c193e6dcdb878a58fa4322c1932bb13af5cf
SimonDevon <simon@simondevon.co.uk>
2017-06-06 01:24:40 +0100

    Added first star script

---
+++ star1.py
@@ -0,0 +1,16 @@
+# Step 1: Make all the "turtle" commands available to us.
+import turtle
+
+# Step 2: create a new turtle, we'll call him bob and make him look like a turtle
+bob = turtle.Turtle()
+bob.shape("turtle")
+
+# Step 3: lets make Bob draw a red star
+bob.color("red")
+
+# Lets draw a star!
+for i in range(5):
+    bob.forward(200)
+    bob.right(144)
+    
+
